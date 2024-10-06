0e9a017de476080880ece2fec06a38f6aa7bfc7b
timoteo dinelli <timoteo.dinelli@polimi.it>
2024-10-06 16:16:12 +0200

    Refit update

--- diffPLOG2TROE/refitter.py
+++ diffPLOG2TROE/refitter.py
@@ -1,5 +1,3 @@
-# -----------------------------------------------------------
-# Modules
 from functools import partial
 from typing import NamedTuple
 # JAX
@@ -15,8 +13,6 @@ from .pressure_logarithmic import compute_plog, kinetic_constant_plog
 from .falloff import compute_falloff
 
 
-# -----------------------------------------------------------
-# Fist guess computation
 def compute_pressure_limits(plog: jnp.ndarray, T_range: jnp.ndarray, P_range: jnp.ndarray) -> jnp.ndarray:
     """
     This function compute the First guess Arrhenius parameters for the HPL and LPL. The computation is done in such a
@@ -39,17 +35,15 @@ def compute_pressure_limits(plog: jnp.ndarray, T_range: jnp.ndarray, P_range: jn
     fg = fg.at[4].set(bInf_fg - 1)
     fg = fg.at[5].set(EaInf_fg)
 
-    # print(" Computing first guesses for the LPL and HPL")
-    # print("  * Adjusted R2 for the LPL: {:.7}".format(R2adj0))
-    # print("    - A: {:.7e}, b: {:.7}, Ea: {:.7e}".format(A0_fg, b0_fg, Ea0_fg))
-    # print("  * Adjusted R2 for the HPL: {:.7}".format(R2adjInf))
-    # print("    - A: {:.7e}, b: {:.7}, Ea: {:.7e}\n".format(AInf_fg, bInf_fg, EaInf_fg))
+    print(" Computing first guesses for the LPL and HPL")
+    print("  * Adjusted R2 for the LPL: {:.7}".format(R2adj0))
+    print("    - A: {:.7e}, b: {:.7}, Ea: {:.7e}".format(A0_fg, b0_fg, Ea0_fg))
+    print("  * Adjusted R2 for the HPL: {:.7}".format(R2adjInf))
+    print("    - A: {:.7e}, b: {:.7}, Ea: {:.7e}\n".format(AInf_fg, bInf_fg, EaInf_fg))
 
     return fg
 
 
-# -----------------------------------------------------------
-# Loss function
 @jit
 def rmse_loss_function(x: jnp.ndarray, data: tuple) -> jnp.float64:
     def full_troe() -> tuple:
@@ -128,8 +122,8 @@ def rmse_loss_function(x: jnp.ndarray, data: tuple) -> jnp.float64:
 
 
     # Regularization terms, L1 and L2 norms
-    l2_regularization = 0.0 * jnp.sum(x ** 2)
-    l1_regularization = 0.0 * jnp.sum(jnp.abs(x))
+    l2_regularization = .0 * jnp.sum(x ** 2)
+    l1_regularization = .0 * jnp.sum(jnp.abs(x))
 
     # Total loss is RMSE + regularization term
     total_loss = l2_loss + l2_regularization + l1_regularization
@@ -147,15 +141,7 @@ def nlopt_loss(x, loss_gradient, data, iteration_count):
 
     iteration_count[0] += 1
     if iteration_count[0] % 10 == 0:
-        print(f"Loss = {loss:.10E}")
-        # print(f"Iteration {iteration_count[0]}: Loss = {loss:.10E}")
-        # A0, b0, Ea0 = jnp.exp(x[3]), x[4], x[5]*1.987
-        # AInf, bInf, EaInf = jnp.exp(x[0]), x[1], x[2]*1.987
-        # A, T3, T1, T2 = x[6], x[7], x[8], x[9]
-        # A, T3, T1, T2 = x[0], x[1], x[2], x[3]
-        # print(f" LPL:  {A0:.5E},   {b0:.5E},   {Ea0:.5E}")
-        # print(f" HPL:  {AInf:.5E}, {bInf:.5E}, {EaInf:.5E}")
-        # print(f" TROE: {A:.5E},    {T3:.5E},   {T1:.5E}, {T2:.5E}")
+        print(f"Iteration: {iteration_count[0]}, Value: {loss}")
     return float(loss)
 
 
@@ -195,8 +181,8 @@ def run_lbfgs(init_params, fun, opt, max_iter, tol, *args):
 
     def continuing_criterion(carry):
         _, state = carry
-        iter_num = otu.tree_get(state, 'count')
-        grad = otu.tree_get(state, 'grad')
+        iter_num = otu.tree_get(state, "count")
+        grad = otu.tree_get(state, "grad")
         err = otu.tree_l2_norm(grad)
         return (iter_num == 0) | ((iter_num < max_iter) & (err >= tol))
 
@@ -221,7 +207,7 @@ def print_info(frequency=1):
 
         def print_fn(_):
             debug.print(
-                'Iteration: {i}, Value: {v}, Gradient norm: {e}',
+                "Iteration: {i}, Value: {v}, Gradient norm: {e}",
                 i=state.iter_num,
                 v=value,
                 e=otu.tree_l2_norm(grad)
