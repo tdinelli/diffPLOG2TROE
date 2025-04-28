8749282437053ba0f159f1dd4af3cf23a297bf87
timoteo dinelli <timoteo.dinelli@polimi.it>
2025-04-28 11:12:08 +0200

    workaround for installation

--- diffPLOG2TROE/__init__.py
+++ diffPLOG2TROE/__init__.py
@@ -1,7 +1,7 @@
-import jax
-
-from .__version__ import __version__
-
-
-jax.config.update("jax_enable_x64", True)
-__all__ = ["__version__"]
+# import jax
+#
+# from .__version__ import __version__
+#
+#
+# jax.config.update("jax_enable_x64", True)
+# __all__ = ["__version__"]
