f0f6a9854c544721354cddea8029215888d3734a
timoteo dinelli <timoteo.dinelli@polimi.it>
2025-07-15 14:54:28 +0200

    More on cleaning examples

--- diffPLOG2TROE/__init__.py
+++ diffPLOG2TROE/__init__.py
@@ -1,11 +1,14 @@
 """diffPLOG2TROE - A differentiable PLOG to TROE refitter"""
 
 from __future__ import annotations
-from ._version import version as __version__
+
 import jax
-
-
 jax.config.update("jax_enable_x64", True)
+
+from ._version import version as __version__
+from .utilities.physical_constants import constants
+
+
 
 
 __all__ = ["__version__"]
