652367110523354ee0065441413ba54ef41ee712
timoteo dinelli <timoteo.dinelli@polimi.it>
2025-11-25 10:23:36 +0100

    Removed import statement

--- KiRATE/__init__.py
+++ KiRATE/__init__.py
@@ -6,7 +6,6 @@ import jax
 jax.config.update("jax_enable_x64", True)
 
 from ._version import version as __version__
-from .utilities.physical_constants import constants
 
 
 __all__ = ["__version__"]
