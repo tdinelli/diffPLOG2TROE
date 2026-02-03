eee81337f7e4fc553aed1447d271f1a4c85438a7
timoteo dinelli <timoteo.dinelli@polimi.it>
2026-02-03 23:26:58 +0100

    Add copyright headers and clean up formatting in package root
      - Add copyright headers to __init__.py and _version.py
      - Remove extra blank lines in __init__.py
      - Update version file docstring format

--- KiRATE/__init__.py
+++ KiRATE/__init__.py
@@ -1,11 +1,14 @@
+"""
+Copyright (c) 2024-2026 Timoteo Dinelli
+Licensed under the MIT License - see LICENSE file for details
+"""
+
 from __future__ import annotations
 
 import jax
 
-
 jax.config.update("jax_enable_x64", True)
 
 from ._version import version as __version__
-
 
 __all__ = ["__version__"]
--- KiRATE/_version.py
+++ KiRATE/_version.py
@@ -1,3 +1,7 @@
-"""Version information."""
+"""
+Copyright (c) 2024-2026 Timoteo Dinelli
+Licensed under the MIT License - see LICENSE file for details
+"""
 
+# Version information
 version = "1.0.0"
